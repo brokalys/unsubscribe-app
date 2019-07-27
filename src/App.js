85e07f093e08781e4355551916e08a2d0a4db59d
Matiss Janis Aboltins <matiss@mja.lv>
2019-07-27 20:52:17 +0300

    Get key & id from querystring

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import axios from 'axios';
+import queryString from 'query-string';
 import './App.css';
 
 class App extends React.Component {
   constructor() {
     super();
+
+    const params = queryString.parse(window.location.search);
+
     this.state = {
       success: false,
       error: false,
+      key: params.key,
+      id: params.id,
     };
 
     this.unsubscribe = this.unsubscribe.bind(this);
   }
 
   async unsubscribe() {
-    const [,, key, id] = window.location.pathname.split('/');
-
     try {
       await axios.post('https://api.brokalys.com/', {
         query: `mutation {
-          unsubscribePinger(id: ${id}, unsubscribe_key: "${key}")
+          unsubscribePinger(id: ${this.state.id}, unsubscribe_key: "${this.state.key}")
         }`
       });
 
@@ -30,6 +34,12 @@ class App extends React.Component {
   }
 
   render() {
+    if (!this.state.key || !this.state.id) {
+      return (
+        <p className="feedback-error">Invalid unsubscribe link.</p>
+      );
+    }
+
     if (this.state.success) {
       return (
         <p className="feedback-success">Unsubscribed succesfully.</p>
