939a28965efaa08c8e01c060b9cd558027ac64a5
Matiss Janis Aboltins <matiss@mja.lv>
2019-09-07 18:25:28 +0300

    Updates

---
+++ precache-manifest.380c081bbd74c265400d1ec212b8d778.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bc9099f58ee0722b8ad070b6cd7dd14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c114a21b389b75bbd238",
+    "url": "/static/css/main.3e52f998.chunk.css"
+  },
+  {
+    "revision": "666797ab90d12876546f",
+    "url": "/static/js/2.265fba03.chunk.js"
+  },
+  {
+    "revision": "c114a21b389b75bbd238",
+    "url": "/static/js/main.363f4582.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ea1213cef26402f6bcd1a7fe72437887.js"
+  "/precache-manifest.380c081bbd74c265400d1ec212b8d778.js"
 );
 
 self.addEventListener('message', (event) => {
